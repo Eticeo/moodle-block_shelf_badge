12dc03fd57b92410a1a548c3c2302df594b2f511
DevEticeo <107034564+DevEticeo@users.noreply.github.com>
2023-02-13 10:57:59 +0100

    v1.2

--- version.php
+++ version.php
@@ -1,10 +1,21 @@
 <?php
-/**
- * Copyright (c) 2021. Eticeo - https://eticeo.com
- */
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Bibliothèque des badges d'un user
+ * Defines the version of shelf_badges
  *
  * @package    block_shelf_badge
  * @copyright  2021 Eticeo <contact@eticeo.fr>
@@ -17,10 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2021042801;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2017110800;        // Requires this Moodle version
-$plugin->release = '1.1';
+$plugin->release   = '1.2';
 $plugin->component = 'block_shelf_badge'; // Full name of the plugin (used for diagnostics)
-$plugin->cron      = 24*3600;           // Cron interval 1 day.
-
-$plugin->dependencies = [
-	'report_indicators' => ANY_VERSION
-];
